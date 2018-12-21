7ebaf93bc8f60ab9a0457ac4fe2a408c3b39c420
ishaaaqh <mohammedishaandsp@gmail.com>
2018-12-21 20:51:08 +0530

    added export csv functionality to the data

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -8,10 +8,21 @@ class ScraperController < ApplicationController
 
   def get_7days_data
     @seven_day_data = Scraper.all.where(date: (Time.now - 7.day)..Time.now)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @seven_day_data.to_csv }
+    end
   end
 
   def get_30days_data
     @thirty_day_data = Scraper.all.where(date: (Time.now - 30.day)..Time.now)
-  end
 
+    respond_to do |format|
+      format.html
+      format.csv { send_data @thirty_day_data .to_csv }
+    end
+  end
 end
+
+
